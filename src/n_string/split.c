9bd02ae1af8ec0b3c02bc6dc034e2f84877a985f
Nick Gerrets <ngerrets@student.codam.nl>
2022-09-01 17:46:32 +0200

    put split in separate file

---
+++ src/n_string/split.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   split.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ngerrets <ngerrets@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/01 17:45:39 by ngerrets      #+#    #+#                 */
+/*   Updated: 2022/09/01 17:45:40 by ngerrets      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "n_string.h"
+
+static char	*_alloc_word(const char *str, const char *delims)
+{
+	size_t	i;
+	size_t	len;
+	char	*r;
+
+	len = 0;
+	while (str[len] && !n_strhaschar(delims, str[len]))
+		len++;
+	r = malloc(sizeof(char) * (len + 1));
+	if (!r)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		r[i] = str[i];
+		i++;
+	}
+	r[i] = '\0';
+	return (r);
+}
+
+static void	_fill_array(char **arr,const char *str, const char *delims, size_t words)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < words)
+	{
+		str = n_str_first_word(str, delims);
+		arr[i] = _alloc_word(str, delims);
+		if (!arr[i])
+		{
+			arr[i] = NULL;
+			n_strarr_free(arr);
+			return (NULL);
+		}
+		while (*str && !n_strhaschar(delims, *str))
+			str++;
+		i++;
+	}
+	arr[i] = NULL;
+}
+
+char	**n_split(const char *str, const char *delims)
+{
+	char	**arr;
+	size_t	words;
+	size_t	i;
+
+	words = n_str_count_words(str, delims);
+	arr = malloc(sizeof(char*) * (words + 1));
+	if (!arr)
+		return (NULL);
+	if (words == 0)
+	{
+		*arr = NULL;
+		return (arr);
+	}
+	_fill_array(arr, str, delims, words);
+	return (arr);
+}
--- src/n_string/strarr.c
+++ src/n_string/strarr.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   strarr.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ngerrets <ngerrets@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/01 17:45:47 by ngerrets      #+#    #+#                 */
+/*   Updated: 2022/09/01 17:45:48 by ngerrets      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "n_string.h"
 
 void	n_strarr_exec(char **arr, t_str_f func)
@@ -31,60 +43,4 @@ char	*n_str_first_word(const char *str, const char *delims)
 	while (n_strhaschar(delims, *str))
 		str++;
 	return ((char *)str);
-}
-
-static char	*_alloc_word(const char *str, const char *delims)
-{
-	size_t	i;
-	size_t	len;
-	char	*r;
-
-	len = 0;
-	while (str[len] && !n_strhaschar(delims, str[len]))
-		len++;
-	r = malloc(sizeof(char) * (len + 1));
-	if (!r)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		r[i] = str[i];
-		i++;
-	}
-	r[i] = '\0';
-	return (r);
-}
-
-char	**n_split(const char *str, const char *delims)
-{
-	char	**arr;
-	size_t	words;
-	size_t	i;
-
-	words = n_str_count_words(str, delims);
-	arr = malloc(sizeof(char*) * (words + 1));
-	if (!arr)
-		return (NULL);
-	if (words == 0)
-	{
-		*arr = NULL;
-		return (arr);
-	}
-	i = 0;
-	while (i < words)
-	{
-		str = n_str_first_word(str, delims);
-		arr[i] = _alloc_word(str, delims);
-		if (!arr[i])
-		{
-			arr[i] = NULL;
-			n_strarr_free(arr);
-			return (NULL);
-		}
-		while (*str && !n_strhaschar(delims, *str))
-			str++;
-		i++;
-	}
-	arr[i] = NULL;
-	return (arr);
 }

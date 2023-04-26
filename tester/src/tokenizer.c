a9ac84dc52036c6c4bd65cc4d81b318dbb8e478a
Nick <nick@pop-os.localdomain>
2023-04-26 23:33:37 +0200

    next encap token

--- tester/src/tokenizer.c
+++ tester/src/tokenizer.c
@@ -84,6 +84,33 @@ t_list *token_get_encapsulate(char const *str, const char *left, const char *rig
 	return (list);
 }
 
+char *token_next_encapsulated(char **str, const char *left, const char *right)
+{
+	char	*l;
+	char	*r;
+
+	l = n_strfind(*str, left);
+	if (!l)
+		return (NULL);
+	r = l;
+	r = n_strfind(r + 1, right);
+	char *f = l;
+	while (f)
+	{
+		f = n_strfind(f + 1, left);
+		if (f && f < r)
+			r = n_strfind(r + 1, right);
+	}
+	if (!r)
+		return (NULL);
+	*str = r + 1;
+	t_string_window strwin = n_string_window_create_p(l + 1, r);
+	if (strwin.length > 0)
+		return (n_string_window_dup(strwin));
+	return (NULL);
+}
+
+
 static void list_print(t_list *element)
 {
 	printf("(%s)->", (char *)element->content);
@@ -105,12 +132,19 @@ void tester_tokenizer(void)
 
 	t_list *split = token_get_split(str, "=");
 
-	t_list *encap = token_get_encapsulate(str, "{", "}");
+	// t_list *encap = token_get_encapsulate(str, "{", "}");
 
 	IND; n_list_iterate(split, list_print); printf("\n");
-	IND; n_list_iterate(encap, list_print); printf("\n");
+	// IND; n_list_iterate(encap, list_print); printf("\n");
+
+	char *p = str;
+	char *tok = token_next_encapsulated(&p, "{", "}");
+
+	IND; n_putstr_endl(tok);
+
+	free(tok);
 
 	n_list_free(split, free);
-	n_list_free(encap, free);
+	// n_list_free(encap, free);
 
 }

66d9e2461a52e10fc8aee4d9bf1c8b9b1ebe8294
Nick Gerrets <ngerrets@student.codam.nl>
2023-06-05 03:06:31 +0200

    now using djb2 hash function

--- src/hashmap/hashmap.c
+++ src/hashmap/hashmap.c
@@ -8,14 +8,12 @@
 /* Polynomial rolling hash function for C-strings */
 uint64_t hash_cstr(char const *str)
 {
-	uint64_t hash = 0;
-	uint64_t p = 31;
-	uint64_t m = 1e9 + 7;
-	uint64_t p_pow = 1;
-	for (size_t i = 0; i < strlen(str); ++i)
+	uint64_t hash = 5381;
+
+	while (*str)
 	{
-		hash = (hash * (str[i] - 'a' + 1) * p_pow) % m;
-		p_pow = (p_pow * p) % m;
+		hash = ((hash << 5) + hash) + *str;
+		++str;
 	}
 	return (hash);
 }
